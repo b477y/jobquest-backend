608c94e69cacfa67479f72a35ec5d95176ba2ec5
Mohammed Saleh <mohammedsaleh002@outlook.com>
2025-02-24 21:06:54 +0200

    feat(auth): add email confirmation service - Implemented `confirmEmail.service.js` for handling email confirmation - Updated `auth.controller.js` to integrate the confirmation logic

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import signUp from "./services/signup.service.js";
+import confirmEmail from "./services/confirmEmail.service.js";
 import { validation } from "../../middlewares/validation.middleware.js";
 import * as validators from "./auth.validation.js";
 
 const router = new Router();
 
 router.post("/signup", validation(validators.signUp), signUp);
+router.patch("/confirm-email", confirmEmail);
 
 export default router;
---
+++ src/modules/auth/services/confirmEmail.service.js
@@ -0,0 +1,50 @@
+import asyncHandler from "../../../utils/response/error.response.js";
+import UserModel from "../../../db/models/user.model.js";
+import * as dbService from "../../../db/db.service.js";
+import successResponse from "../../../utils/response/success.response.js";
+import { OtpTypes } from "../../../utils/enum/enum.js";
+import { compareHash } from "../../../utils/security/hash.security.js";
+
+const confirmEmail = asyncHandler(async (req, res, next) => {
+  const { email, OTP } = req.body;
+
+  const user = await dbService.findOne({
+    model: UserModel,
+    filter: { email },
+  });
+
+  if (!user || !user.OTP.length) {
+    return next(new Error("Invalid OTP or user not found", 400));
+  }
+
+  const storedOtp = user.OTP.find((otp) => otp.type === OtpTypes.CONFIRM_EMAIL);
+
+  if (!storedOtp) {
+    return next(new Error("No confirmation OTP found", 400));
+  }
+
+  if (new Date(storedOtp.expiresIn) < new Date()) {
+    return next(new Error("OTP has expired", 400));
+  }
+
+  const isMatch = await compareHash({
+    plaintext: OTP,
+    encryptedText: storedOtp.code,
+  });
+
+  if (!isMatch) {
+    return next(new Error("Invalid OTP", 400));
+  }
+
+  user.isConfirmed = true;
+  user.OTP = user.OTP.filter((otp) => otp.type !== OtpTypes.CONFIRM_EMAIL);
+  await user.save();
+
+  return successResponse({
+    res,
+    status: 200,
+    message: "Email successfully confirmed",
+  });
+});
+
+export default confirmEmail;
