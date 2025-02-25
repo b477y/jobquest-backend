c8f897fe4b02247a11b09770183000046495c044
Mohammed Saleh <mohammedsaleh002@outlook.com>
2025-02-25 12:32:38 +0200

    feat(auth): Implement reset password functionality
    - Added `resetPassword.service.js` to handle password reset requests.
    - Updated `auth.controller.js` to integrate reset password logic.
    - Modified `confirmEmail.service.js` to align with reset flow.
    - Ensured OTP validation, expiration checks, and password updates.
    - Password is now securely hashed using Mongoose hooks.

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -6,6 +6,7 @@ import * as validators from "./auth.validation.js";
 import signIn from "./services/signin.service.js";
 import signInWithGoogle from "./services/googleOAuth.service.js";
 import forgotPassword from "./services/forgotPassword.service.js";
+import resetPassword from "./services/resetPassword.service.js";
 
 const router = new Router();
 
@@ -14,5 +15,6 @@ router.patch("/confirm-email", confirmEmail);
 router.post("/signin", signIn);
 router.post("/signin-with-google", signInWithGoogle);
 router.post("/forgot-password", forgotPassword);
+router.patch("/reset-password", resetPassword);
 
 export default router;
--- src/modules/auth/services/resetPassword.service.js
+++ src/modules/auth/services/resetPassword.service.js
@@ -5,8 +5,12 @@ import successResponse from "../../../utils/response/success.response.js";
 import { OtpTypes } from "../../../utils/enum/enum.js";
 import { compareHash } from "../../../utils/security/hash.security.js";
 
-const confirmEmail = asyncHandler(async (req, res, next) => {
-  const { email, OTP } = req.body;
+const resetPassword = asyncHandler(async (req, res, next) => {
+  const { email, OTP, password, confirmationPassword } = req.body;
+
+  if (password !== confirmationPassword) {
+    return next(new Error("Passwords do not match", 400));
+  }
 
   const user = await dbService.findOne({
     model: UserModel,
@@ -17,7 +21,9 @@ const confirmEmail = asyncHandler(async (req, res, next) => {
     return next(new Error("Invalid OTP or user not found", 400));
   }
 
-  const storedOtp = user.OTP.find((otp) => otp.type === OtpTypes.CONFIRM_EMAIL);
+  const storedOtp = user.OTP.find(
+    (otp) => otp.type === OtpTypes.FORGOT_PASSWORD
+  );
 
   if (!storedOtp) {
     return next(new Error("No OTP found", 400));
@@ -36,15 +42,16 @@ const confirmEmail = asyncHandler(async (req, res, next) => {
     return next(new Error("Invalid OTP", 400));
   }
 
-  user.isConfirmed = true;
-  user.OTP = user.OTP.filter((otp) => otp.type !== OtpTypes.CONFIRM_EMAIL);
+  user.password = password;
+
+  user.OTP = user.OTP.filter((otp) => otp.type !== OtpTypes.FORGOT_PASSWORD);
   await user.save();
 
   return successResponse({
     res,
     status: 200,
-    message: "Email successfully confirmed",
+    message: "Password reset successfully",
   });
 });
 
-export default confirmEmail;
+export default resetPassword;
